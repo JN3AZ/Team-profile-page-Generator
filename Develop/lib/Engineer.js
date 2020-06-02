cf70a77c495c3021b95dc0e73f74abe6d70a47bf
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-01 23:45:58 -0700

    set up the Engineer class and had it extend from the employee class , also set up the get methods for engineer.js file

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+    constructor(id, name, email, github) {
+        super(id, name, email);
+        this.title = 'Engineer';
+        this.github = github
+    }
+
+     //getter methods
+    getRole() {
+        return this.title;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+}
+
+module.exports = Engineer;
