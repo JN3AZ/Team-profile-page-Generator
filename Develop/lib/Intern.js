203b6d171147b444956a4d6def22009a4dcf1882
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-01 23:55:13 -0700

    set up the intern class and made it extend from the employee class, created the get methods  and set up the module.export to Intern

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Intern extends Employee {
+    constructor(id, name, email) {
+    super(id, name, email);
+    this.title = 'Intern';
+    this.school = school
+  }
+
+  getRole() {
+      return this.title;
+  }
+
+  getSchool() {
+      return this.school
+  }
+}
+
+module.exports = Intern;
